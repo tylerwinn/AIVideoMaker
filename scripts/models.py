fa5377fd7a99f52c1d57003ca29c4427a6f8b61d
Tyler <128875619+tylerwinn@users.noreply.github.com>
2023-06-15 18:00:13 -0500

    Update models.py

--- scripts/models.py
+++ scripts/models.py
@@ -1,6 +1,6 @@
 import os
 import openai
-openai.api_key = "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"
+openai.api_key = ""
 models = openai.Model.list()
 
 root_values = [model['root'] for model in models['data']]
